5bfa6b3cbf0c437e4565acb8d52d45aee511f14c
piyush <pdaga3@gmail.com>
2023-01-07 23:05:31 -0500

    Change 2 commit

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -11,6 +11,6 @@ class MainActivity : AppCompatActivity() {
         println("This is contained in second Commit")
         println("Local Changes")
 
-        println("Experimental")
+        println("Change 2")
     }
 }
